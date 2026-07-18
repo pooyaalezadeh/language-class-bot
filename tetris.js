b052869ce5fff70795c331e780315847262d1ad5
pooyaalezadeh <poya1385ach@gmail.com>
2026-07-18 06:32:47 -0700

    Add files via upload

---
+++ tetris.js
@@ -0,0 +1,14 @@
+module.exports = function(){
+
+    return `
+    🎮 بازی تتریس زبان انگلیسی
+    
+    برای شروع بازی روی لینک زیر بزنید:
+    
+    https://language-class-bot-2.onrender.com/games/tetris/index.html
+    
+    🎯 هدف:
+    با بازی کردن لغات انگلیسی را تمرین کن و امتیاز بگیر.
+    `;
+    
+    };
