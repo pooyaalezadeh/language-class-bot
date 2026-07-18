fcd281228ebe01bc752b0b10dee40d09fa7a6005
pooyaalezadeh <poya1385ach@gmail.com>
2026-07-18 02:06:45 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -1,63 +1,35 @@
-const words = require("./words");
-
-class Game {
-
-    constructor(send){
-        this.send = send;
-        this.players = {};
+const questions = [
+    {
+    q:"Apple یعنی؟",
+    a:"سیب"
+    },
+    {
+    q:"Book یعنی؟",
+    a:"کتاب"
+    },
+    {
+    q:"House یعنی؟",
+    a:"خانه"
     }
-
-    async start(chatId){
-
-        const random =
-            words[Math.floor(Math.random() * words.length)];
-
-        this.players[chatId] = random;
-
-        await this.send(
-            chatId,
-`🎮 بازی لغات
-
-🇬🇧 معنی این کلمه چیست؟
-
-${random.word}
-
-✍️ پاسخ را ارسال کنید.`
-        );
-
-    }
-
-    async check(chatId, text){
-
-        if(!this.players[chatId]){
-            return false;
-        }
-
-        const question = this.players[chatId];
-
-        if(text.trim() === question.answer){
-
-            delete this.players[chatId];
-
-            await this.send(
-                chatId,
-"🎉 آفرین!\n\n✅ پاسخ صحیح بود."
-            );
-
-        }else{
-
-            await this.send(
-                chatId,
-`❌ پاسخ اشتباه است.
-
-دوباره تلاش کن.`
-            );
-
-        }
-
-        return true;
-    }
-
-}
-
-module.exports = Game;
+    ];
+    
+    
+    module.exports = function game(){
+    
+    let item =
+    questions[
+    Math.floor(Math.random()*questions.length)
+    ];
+    
+    
+    return `
+    🎮 بازی زبان
+    
+    ترجمه کن:
+    
+    🇬🇧 ${item.q}
+    
+    جواب را ارسال کن 😊
+    `;
+    
+    };
