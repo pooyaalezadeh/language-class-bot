b38d8160a9140dcf3d891baa9eb7b6a535601767
pooyaalezadeh <poya1385ach@gmail.com>
2026-07-18 02:07:43 -0700

    Add files via upload

---
+++ missions.js
@@ -0,0 +1,21 @@
+const missions = {
+
+    dailyLesson:{
+    title:"📖 تمرین روزانه",
+    xp:10
+    },
+    
+    game:{
+    title:"🎮 بازی زبان",
+    xp:15
+    },
+    
+    profile:{
+    title:"👤 مشاهده پروفایل",
+    xp:5
+    }
+    
+    };
+    
+    
+    module.exports = missions;
